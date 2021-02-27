7d8ec6876b471595d5bba59f24d9ef44251dfc9a
CrazyXumuk <nidervv@mail.ru>
2021-02-27 22:21:45 +0300

    2. Реализовать функцию, принимающую несколько параметров, описывающих данные пользователя: имя, фамилия, год рождения, город проживания, email, телефон. Функция должна принимать параметры как именованные аргументы. Реализовать вывод данных о пользователе одной строкой.

---
+++ 3-2.py
@@ -0,0 +1,4 @@
+def my_func(name, surname, year, city, email, phone):
+    return f"{name}, \n{surname}, \n{city}, \n{year}, \n{email}, \n{phone}"
+print(my_func('Виктория', 'Нидер', 'St.Petersburg', '19.12.1988', 'nider@mail.ru', '89003447722'))
+
